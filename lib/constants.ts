6ed96d1a6d3e650edd8a06e51ee80e0d509bd9c8
chmst-dev <ochimasato@gmail.com>
2026-05-13 22:24:46 +0900

    Update constants.ts

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,5 +1,5 @@
 export const SITE_NAME = "一般社団法人オンザライス";
 export const SITE_NAME_EN = "On The Rice";
 export const SITE_TAGLINE = "おいしい、たのしい、ごはんがすすむ。その感動を、文化にする。";
-export const URL_GOOGLE_FORM = "https://forms.gle/XXXXXXXXXXXX";
-export const URL_NOTE = "https://note.com/ontherice";
+export const URL_GOOGLE_FORM = "https://docs.google.com/forms/d/e/1FAIpQLScHu7MsVh0pKCvXSfz3F85joNLKFRVCcScrCtqgrckvXfgFeA/viewform";
+export const URL_NOTE = "https://note.com/";
