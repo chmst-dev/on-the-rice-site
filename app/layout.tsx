a34cef1c54494de11054e61b0c17eefad1ab281d
ochi <ochimasato@gmail.com>
2026-06-16 18:00:33 +0900

    chore: Google Analyticsタグを追加

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import { SITE_NAME, SITE_TAGLINE } from "@/lib/constants";
+import Script from "next/script";
+
 
 export const metadata: Metadata = {
   title: {
@@ -19,6 +21,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja">
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-SZMS1YMYQX"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-SZMS1YMYQX');
+          `}
+        </Script>
+      </head>
       <body className="min-h-screen flex flex-col">
         <Header />
         <main className="flex-grow">{children}</main>
